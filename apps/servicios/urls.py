d474cb4c589d8152728b0526963ca54daff80c23
engeldev <engeljavierpinto@gmail.com>
2018-09-02 21:39:32 -0400

    Se añadió la funcionalidad de add un nuevo servicio

--- apps/servicios/urls.py
+++ apps/servicios/urls.py
@@ -5,7 +5,8 @@ from .views import *
 app_name = "servicios" 
 
 urlpatterns = [
-	path('servicios/', ServiciosList.as_view(), name="servicios"),
-	path('servicios/<int:pk>/editar', ServicioUpdate.as_view(), name="editar_servicio"),
+	path('servicios/', ServiciosList.as_view(), name="index"),
+	path('crear/', ServicioCreate.as_view(), name='crear_servicio'),
+	path('servicios/<int:pk>/editar/', ServicioUpdate.as_view(), name="editar_servicio"),
 	path('horarios/', HorarioList.as_view(), name="horario"),
-] 
+] 	
--- apps/servicios/views.py
+++ apps/servicios/views.py
@@ -6,6 +6,13 @@ from .models import *
 from .forms import RegistrarServicio
 
 
+class ServicioCreate(generic.CreateView):
+	model = Servicio
+	template_name = 'servicios/registrar_servicio.html'
+	form_class = RegistrarServicio
+	success_url = reverse_lazy("servicios:index")
+
+
 class ServiciosList(generic.ListView):
 	model = Servicio
 	context_object_name = "servicios"
@@ -16,9 +23,9 @@ class ServiciosList(generic.ListView):
 class ServicioUpdate(generic.UpdateView):
 	model = Servicio
 	form_class = RegistrarServicio
-	template_name = 'servicios/registrar_servicio.html'
+	template_name = 'servicios/editar_servicio.html'
 	context_object_name = 'servicio'
-	success_url = reverse_lazy('servicios:servicios') 	
+	success_url = reverse_lazy('servicios:index') 	
 
 
 class HorarioList(generic.ListView):
