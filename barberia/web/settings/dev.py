9c0256742a96c28601884a9c84c1224b4cc996f4
engeldev <engeljavierpinto@gmail.com>
2019-05-24 11:55:31 -0400

    Agregre la configuración del archivo wsgi.py con el servidor apache.

--- barberia/web/settings/dev.py
+++ barberia/web/settings/dev.py
@@ -8,6 +8,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.sqlite3',
 		'NAME': dir_root('db.sqlite3'),
 	}
+}
 
 
 STATICFILES_DIRS = [dir_root('static')]
--- barberia/web/wsgi.py
+++ barberia/web/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for web project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings.prod")
-
-application = get_wsgi_application()
+import os  
+import sys 
+ 
+from django.core.wsgi import get_wsgi_application 
+ 
+sys.path.append('/var/www/html/barberia/barberia') 
+# adjust the Python version in the line below as needed 
+sys.path.append('/var/www/html/barberia/env/lib/python3.5/site-packages') 
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings.prod") 
+  
+application = get_wsgi_application() 
