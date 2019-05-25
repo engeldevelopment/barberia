24dd20436ff63d158f279f7d20060bd4da984ffe
engeldev <engeljavierpinto@gmail.com>
2019-05-25 13:54:55 -0400

    Agregué el campo de imagen en el formulario de servicio. También, al editar se muestra la foto del servicio. Ahora sólo se muestran los botones de editar y eliminar al super usuario.

--- barberia/apps/servicios/forms.py
+++ barberia/apps/servicios/forms.py
@@ -6,13 +6,20 @@ class ServicioForm(forms.ModelForm):
 
 	class Meta:
 		model = Servicio
-		fields = ['descripcion', 'precio']
+		fields = ['descripcion', 'precio', 'foto']
 		labels = {
 			'descripcion': 'Descripción',
 			'precio': 'Precio',
+			'foto': "Elige una foto"
 		}
 		widgets = {
 			'descripcion': forms.TextInput(attrs={'class': 'form-control'}),
-			'precio': forms.NumberInput(attrs={'class': 'form-control'}),	
+			'precio': forms.NumberInput(attrs={'class': 'form-control'}),
+			'foto': forms.FileInput(
+					attrs={
+						'class': 'form-control',
+						'accept': 'image/*'
+					}
+				)
 		}
 
