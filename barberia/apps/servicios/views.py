86f9832e4b42546318cd072fb519f2c490c90df7
engeldev <engeljavierpinto@gmail.com>
2019-05-25 14:29:24 -0400

    Sólo los usuarios con permisos pueden editar y eliminar un servicio.

--- barberia/apps/servicios/views.py
+++ barberia/apps/servicios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from .models import *
 from .forms import ServicioForm
@@ -21,20 +21,25 @@ class ServiciosListView(generic.ListView):
 	queryset = Servicio.objects.all().order_by('-precio')
 
 
-class ServicioUpdateView(generic.UpdateView):
+class ServicioUpdateView(LoginRequiredMixin,
+						PermissionRequiredMixin,
+						generic.UpdateView):
 	model = Servicio
 	form_class = ServicioForm
+	permission_required = ('servicios.edit_servicio',)
 	template_name = 'servicios/servicio_form.html'
 	context_object_name = 'servicio'
 	success_url = reverse_lazy('servicios:index')
 
 
-class ServicioDeleteView(generic.DeleteView):
+class ServicioDeleteView(LoginRequiredMixin,
+						PermissionRequiredMixin,
+						generic.DeleteView):
 	model = Servicio
 	template_name = 'servicios/eliminar_servicio.html'
 	context_object_name = 'servicio'
 	success_url = reverse_lazy('servicios:index') 		
-
+	permission_required = ('servicios.delete_servicio')
 
 class HorarioListView(generic.ListView):
 	model = Horario
