68760f9dd0670b6a3a73dd0075767255de239e2f
Engel Javier Pinto Ruiz <engeljavierpinto@gmail.com>
2021-02-06 12:53:55 -0400

    refactor: Agregué BarberosFactory a estos tests.

--- apps/barberos/tests/test_views.py
+++ apps/barberos/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
-from ..models import Barbero
+
+from ..factories import BarberosFactory
 
 
 class BarberoViewTest(TestCase):
@@ -9,26 +10,25 @@ class BarberoViewTest(TestCase):
 	def setUp(self):
 
 		self.engel12 = User.objects.create_user(
-				username="engel12", 
+				username="engel12",
 				password="1234"
 			)
 
-		self.barbero = Barbero.objects.create(
-				cedula="22387256",
-				apodo="El varón",
+		self.barbero = BarberosFactory(
 				usuario=self.engel12
 			)
 
 	def test_cambiar_status_de_usuario_logueado_activo_cambiara_a_inactivo(self):
 
-		url =  reverse('barberos:cambiar_status', 
+		url =  reverse('barberos:cambiar_status',
 			kwargs={'pk': self.barbero.pk})
 
 		self.client.login(
-			username=self.engel12.username, 
+			username=self.engel12.username,
 			password="1234"
 			)
 
 		response = self.client.post(url)
 
 		self.assertEqual(200, response.status_code)
+
